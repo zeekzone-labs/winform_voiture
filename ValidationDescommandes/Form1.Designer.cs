9c3f8f9dbb296a5560386db5125fec9456d28d46
Zeek Zone <74927544+Zeek-Zone@users.noreply.github.com>
2025-12-30 03:22:32 +0100

    Add files via upload

---
+++ ValidationDescommandes/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ValidationDescommandes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbClient = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BtnCmd = new System.Windows.Forms.Button();
+            this.BtnValider = new System.Windows.Forms.Button();
+            this.txtNumCmd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DGVCmd = new System.Windows.Forms.DataGridView();
+            this.BtnArticlesCmd = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.DGVArticlesCmd = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCmd)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVArticlesCmd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client";
+            // 
+            // cbClient
+            // 
+            this.cbClient.FormattingEnabled = true;
+            this.cbClient.Location = new System.Drawing.Point(42, 25);
+            this.cbClient.Name = "cbClient";
+            this.cbClient.Size = new System.Drawing.Size(121, 21);
+            this.cbClient.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbClient);
+            this.panel1.Location = new System.Drawing.Point(29, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 181);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BtnCmd);
+            this.panel2.Controls.Add(this.BtnValider);
+            this.panel2.Controls.Add(this.txtNumCmd);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.DGVCmd);
+            this.panel2.Location = new System.Drawing.Point(207, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(613, 181);
+            this.panel2.TabIndex = 3;
+            // 
+            // BtnCmd
+            // 
+            this.BtnCmd.BackColor = System.Drawing.Color.Turquoise;
+            this.BtnCmd.Location = new System.Drawing.Point(269, 36);
+            this.BtnCmd.Name = "BtnCmd";
+            this.BtnCmd.Size = new System.Drawing.Size(75, 23);
+            this.BtnCmd.TabIndex = 4;
+            this.BtnCmd.Text = "CMDs";
+            this.BtnCmd.UseVisualStyleBackColor = false;
+            this.BtnCmd.Click += new System.EventHandler(this.BtnCmd_Click);
+            // 
+            // BtnValider
+            // 
+            this.BtnValider.BackColor = System.Drawing.Color.Lime;
+            this.BtnValider.Location = new System.Drawing.Point(299, 1);
+            this.BtnValider.Name = "BtnValider";
+            this.BtnValider.Size = new System.Drawing.Size(75, 23);
+            this.BtnValider.TabIndex = 3;
+            this.BtnValider.Text = "Valider";
+            this.BtnValider.UseVisualStyleBackColor = false;
+            this.BtnValider.Click += new System.EventHandler(this.BtnValider_Click);
+            // 
+            // txtNumCmd
+            // 
+            this.txtNumCmd.Location = new System.Drawing.Point(193, 3);
+            this.txtNumCmd.Name = "txtNumCmd";
+            this.txtNumCmd.Size = new System.Drawing.Size(100, 20);
+            this.txtNumCmd.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Num. Cmd";
+            // 
+            // DGVCmd
+            // 
+            this.DGVCmd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVCmd.Location = new System.Drawing.Point(23, 65);
+            this.DGVCmd.Name = "DGVCmd";
+            this.DGVCmd.Size = new System.Drawing.Size(561, 113);
+            this.DGVCmd.TabIndex = 0;
+            // 
+            // BtnArticlesCmd
+            // 
+            this.BtnArticlesCmd.BackColor = System.Drawing.Color.Orange;
+            this.BtnArticlesCmd.Location = new System.Drawing.Point(324, 32);
+            this.BtnArticlesCmd.Name = "BtnArticlesCmd";
+            this.BtnArticlesCmd.Size = new System.Drawing.Size(75, 23);
+            this.BtnArticlesCmd.TabIndex = 4;
+            this.BtnArticlesCmd.Text = "Articles";
+            this.BtnArticlesCmd.UseVisualStyleBackColor = false;
+            this.BtnArticlesCmd.Click += new System.EventHandler(this.BtnArticlesCmd_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.BtnArticlesCmd);
+            this.panel3.Controls.Add(this.DGVArticlesCmd);
+            this.panel3.Location = new System.Drawing.Point(29, 209);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(741, 234);
+            this.panel3.TabIndex = 4;
+            // 
+            // DGVArticlesCmd
+            // 
+            this.DGVArticlesCmd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVArticlesCmd.Location = new System.Drawing.Point(103, 61);
+            this.DGVArticlesCmd.Name = "DGVArticlesCmd";
+            this.DGVArticlesCmd.Size = new System.Drawing.Size(510, 150);
+            this.DGVArticlesCmd.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCmd)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVArticlesCmd)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbClient;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView DGVCmd;
+        private System.Windows.Forms.TextBox txtNumCmd;
+        private System.Windows.Forms.Button BtnValider;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView DGVArticlesCmd;
+        private System.Windows.Forms.Button BtnArticlesCmd;
+        private System.Windows.Forms.Button BtnCmd;
+    }
+}
+
---
+++ ValidationDescommandes/Form1.cs
@@ -0,0 +1,265 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidationDescommandes
+{
+    public partial class Form1 : Form
+    {
+
+        // 1. La chaîne de connexion (À adapter selon votre PC)
+        private readonly string chaineCn = "Host=localhost;Port=5432;Username=postgres;Password=root;Database=commandes";
+
+        // 2. L'objet de connexion Npgsql
+        private readonly NpgsqlConnection cn;
+
+        private NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            cn = new NpgsqlConnection(chaineCn);
+        }
+
+        // Chargement du formulaire (Load)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Remplir_Clients(cbClient);
+            ListeCmd();
+        }
+
+
+        // établit une connexion avec le serveur de base de données et
+        // affiche un message d’erreur en cas d’échec.
+        private void Connecter()
+        {
+            try
+            {
+                if (cn.State != ConnectionState.Open)
+                    cn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connexion à la base de données : " + ex.Message);
+            }
+        }
+
+        // prend en argument un contrôle ComboBox et le remplit par
+        // les noms des clients de la table « Client »
+        private void Remplir_Clients(ComboBox cb)
+        {
+            try
+            {
+                // 1. Connexion à la base de données
+                Connecter();
+
+                // 2. Exécution de la requête
+                // 2.a. Préparation de la requête
+                string requete = "SELECT num, nom FROM client ORDER BY nom";
+
+                // 2.b. Exécution de la requête
+                cmd = new NpgsqlCommand(requete, cn);
+                dr = cmd.ExecuteReader();
+
+                // 3. Remplissage du ComboBox
+                // 3.a. Création d'un DataTable pour stocker les résultats
+                DataTable dt = new DataTable();
+                // 3.b. Chargement des données du DataReader dans le DataTable
+                dt.Load(dr);
+                // 3.c. Liaison du DataTable au ComboBox
+                cb.DataSource = dt;
+
+                // 3.d. Définition des propriétés DisplayMember et ValueMember
+                cb.DisplayMember = "nom"; // Colonne à afficher
+                cb.ValueMember = "num"; // Colonne de la valeur associée
+
+                // 4. Fermeture
+                dr.Close();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du remplissage des clients : " + ex.Message);
+            }
+        }
+
+        // affiche les articles d’une commande.La liste
+        // doit être affichée dans l’objet DataGridView nommé « DGV_Liste » 
+        private void Lister_ArticlesForCmd(string numCmd)
+        {
+            try
+            {
+                // 1. Connexion à la base de données
+                Connecter();
+                // 2. Exécution de la requête
+                // 2.a. Préparation de la requête
+                string requete = @"
+                                    SELECT 
+                                        a.code AS ""Code"", 
+                                        a.libelle AS ""Libelle"", 
+                                        a.prix AS ""Prix"", 
+                                        lc.Qte AS ""Quantité""
+                                    FROM ligneCmd lc
+                                    INNER JOIN article a ON lc.code = a.code
+                                    WHERE lc.numCmd = @numCmd";
+
+                // 2.b. Exécution de la requête
+                cmd = new NpgsqlCommand(requete, cn);
+
+                // 2.c. Ajout des paramètres
+                cmd.Parameters.AddWithValue("numCmd", numCmd);
+                dr = cmd.ExecuteReader();
+
+                // 3. Remplissage du DataGridView
+                // 3.a. Création d'un DataTable pour stocker les résultats
+                DataTable dt = new DataTable();
+
+                // 3.b. Chargement des données du DataReader dans le DataTable
+                dt.Load(dr);
+
+                // 3.c. Liaison du DataTable au DataGridView
+                DGVArticlesCmd.DataSource = dt;
+
+                // 4. Fermeture
+                dr.Close();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la liste des articles pour la commande : " + ex.Message);
+            }
+        }
+
+        // La methode list des commandes (DGVCmd)
+        private void ListeCmd()
+        {
+            try
+            {
+                // Connexion 
+                Connecter();
+
+                string requete = "SELECT * FROM commande ORDER BY numcmd ASC";
+
+                // 2.b. Exécution de la requête
+                cmd = new NpgsqlCommand(requete, cn);
+                dr = cmd.ExecuteReader();
+
+                // 3. Remplissage du DataGridView
+                // 3.a. Création d'un DataTable pour stocker les résultats
+                DataTable dt = new DataTable();
+
+                // 3.b. Chargement des données du DataReader dans le DataTable
+                dt.Load(dr);
+
+                // 3.c. Liaison du DataTable au DataGridView
+                DGVCmd.DataSource = dt;
+
+                // 4. Fermeture
+                dr.Close();
+                cn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la liste des commandes : " + ex.Message);
+            }
+        }
+
+
+
+        // prend en argument le numéro d’une commande
+        // et retourne :
+        //  - « true » s’il existe une ligne de commande correspondant à ce numéro;
+        //  - « false » dans le cas contraire.
+        private bool Existe_Commande(string numCmd)
+        {
+            try
+            {
+                // 1. Connexion à la base de données
+                Connecter();
+                // 2. Exécution de la requête
+                // 2.a. Préparation de la requête
+                string requete = "SELECT COUNT(*) FROM ligneCmd WHERE numCmd = @numCmd";
+                // 2.b. Exécution de la requête
+                cmd = new NpgsqlCommand(requete, cn);
+                // 2.c. Ajout des paramètres
+                cmd.Parameters.AddWithValue("numCmd", numCmd);
+                // 2.d. Exécution de la commande et récupération du résultat
+                object resultat = cmd.ExecuteScalar();
+                int count = Convert.ToInt32(resultat);
+                // 3. Fermeture
+                cn.Close();
+                // 4. Retourne true si count > 0, sinon false
+                return count > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la vérification de l'existence de la commande : " + ex.Message);
+                return false;
+            }
+        }
+
+        // prend en argument le numéro d’une commande et
+        // qui modifie son état avec la valeur « Validée »
+        // et sa date de validation avec la date système de
+        // l’application.
+        private void ValidateCmd(string numCmd)
+        {
+            // Requête SQL de mise à jour
+            string requete = "UPDATE commande SET etat = 'Validée', dateValidation = @dateVal WHERE numCmd = @numCmd";
+
+            // Connexion à la base de données
+            Connecter();
+
+            cmd = new NpgsqlCommand(requete, cn);
+            // Ajout des paramètres
+            cmd.Parameters.AddWithValue("dateVal", DateTime.Now);
+            cmd.Parameters.AddWithValue("numCmd", numCmd);
+            // Exécution de la commande
+            cmd.ExecuteNonQuery();
+            // Fermeture de la connexion
+            cn.Close();
+        }
+
+        private void BtnValider_Click(object sender, EventArgs e)
+        {
+            // Récupération du numéro de commande depuis le TextBox
+            string numCmd = txtNumCmd.Text.Trim();
+            // Vérification de l'existence de la ligne de la commande 
+            if (Existe_Commande(numCmd))
+            {
+                // Validation de la commande
+                ValidateCmd(numCmd);
+                MessageBox.Show("Commande validée avec succès.");
+            }
+            else
+            {
+                MessageBox.Show("La commande n'a pas de ligne de commande associée.");
+            }
+        }
+
+        private void BtnCmd_Click(object sender, EventArgs e)
+        {
+            ListeCmd();
+        }
+
+        private void BtnArticlesCmd_Click(object sender, EventArgs e)
+        {
+            // Récupération du numéro de commande depuis le TextBox
+            string numCmd = txtNumCmd.Text.Trim();
+            Lister_ArticlesForCmd(numCmd);
+        }
+    }
+}
