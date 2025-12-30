c74c491607925d7626f913210d3f46c46d94da6a
Zeek Zone <74927544+Zeek-Zone@users.noreply.github.com>
2025-12-30 01:15:01 +0100

    add hotel files

---
+++ hotel/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿using System.Windows.Forms;
+
+namespace GestionHotel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtPrix = new System.Windows.Forms.TextBox();
+            this.cbEtat = new System.Windows.Forms.ComboBox();
+            this.cbCategorie = new System.Windows.Forms.ComboBox();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.dgvChambres = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChambres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numéro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(136, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prix";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(136, 162);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Etat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(136, 196);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categorie";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(136, 232);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total des chambres";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(328, 232);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(15, 16);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "0";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(224, 76);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(76, 20);
+            this.txtNumero.TabIndex = 6;
+            // 
+            // txtPrix
+            // 
+            this.txtPrix.Location = new System.Drawing.Point(224, 117);
+            this.txtPrix.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPrix.Name = "txtPrix";
+            this.txtPrix.Size = new System.Drawing.Size(76, 20);
+            this.txtPrix.TabIndex = 7;
+            // 
+            // cbEtat
+            // 
+            this.cbEtat.FormattingEnabled = true;
+            this.cbEtat.Location = new System.Drawing.Point(224, 157);
+            this.cbEtat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbEtat.Name = "cbEtat";
+            this.cbEtat.Size = new System.Drawing.Size(92, 21);
+            this.cbEtat.TabIndex = 8;
+            // 
+            // cbCategorie
+            // 
+            this.cbCategorie.FormattingEnabled = true;
+            this.cbCategorie.Location = new System.Drawing.Point(208, 189);
+            this.cbCategorie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbCategorie.Name = "cbCategorie";
+            this.cbCategorie.Size = new System.Drawing.Size(92, 21);
+            this.cbCategorie.TabIndex = 9;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.BackColor = System.Drawing.Color.LightBlue;
+            this.btnRechercher.Location = new System.Drawing.Point(358, 75);
+            this.btnRechercher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(70, 24);
+            this.btnRechercher.TabIndex = 10;
+            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = false;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnAjouter.Location = new System.Drawing.Point(358, 118);
+            this.btnAjouter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(70, 19);
+            this.btnAjouter.TabIndex = 11;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = false;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.BackColor = System.Drawing.Color.Crimson;
+            this.btnSupprimer.Location = new System.Drawing.Point(358, 155);
+            this.btnSupprimer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(70, 20);
+            this.btnSupprimer.TabIndex = 12;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = false;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.BackColor = System.Drawing.Color.Bisque;
+            this.btnModifier.Location = new System.Drawing.Point(358, 193);
+            this.btnModifier.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(70, 19);
+            this.btnModifier.TabIndex = 13;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = false;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // dgvChambres
+            // 
+            this.dgvChambres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChambres.Location = new System.Drawing.Point(139, 284);
+            this.dgvChambres.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvChambres.Name = "dgvChambres";
+            this.dgvChambres.RowTemplate.Height = 24;
+            this.dgvChambres.Size = new System.Drawing.Size(647, 162);
+            this.dgvChambres.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 455);
+            this.Controls.Add(this.dgvChambres);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnRechercher);
+            this.Controls.Add(this.cbCategorie);
+            this.Controls.Add(this.cbEtat);
+            this.Controls.Add(this.txtPrix);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChambres)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label lblTotal;
+        private TextBox txtNumero;
+        private TextBox txtPrix;
+        private ComboBox cbEtat;
+        private ComboBox cbCategorie;
+        private Button btnRechercher;
+        private Button btnAjouter;
+        private Button btnSupprimer;
+        private Button btnModifier;
+        private DataGridView dgvChambres;
+
+
+    }
+}
+
---
+++ hotel/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql; // Nécessaire pour PostgreSQL
+
+namespace GestionHotel
+{
+    public partial class Form1 : Form
+    {
+        // =========================================================
+        // 1. CONFIGURATION CONNEXION
+        // =========================================================
+        // Assure-toi que la Database est bien 'bd_hotel' comme tu l'as dit
+        string strConn = "Host=localhost;Port=5432;Username=postgres;Password=root;Database=bd_hotel";
+        NpgsqlConnection cn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cn = new NpgsqlConnection(strConn);
+        }
+
+        // =========================================================
+        // LOAD : Chargement initial
+        // =========================================================
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ordre important : 
+                // 1. Remplir les listes déroulantes (Etat + Categorie)
+                // 2. Ensuite afficher la grille
+                RemplirComboEtat();
+                RemplirComboCategorie();
+                ActualiserGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur au démarrage : " + ex.Message);
+            }
+        }
+
+        // =========================================================
+        // 2. REMPLIR COMBO CATEGORIE (Table 'categories')
+        // =========================================================
+        public void RemplirComboCategorie()
+        {
+            if (cn.State == ConnectionState.Open) cn.Close();
+            cn.Open();
+
+            // NOM DE TABLE : categories (Pluriel)
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM categories", cn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+
+            cbCategorie.DataSource = dt;
+            cbCategorie.DisplayMember = "nom";
+            cbCategorie.ValueMember = "id_categorie";
+
+            cn.Close();
+        }
+
+        // =========================================================
+        // REMPLIR COMBO ETAT (Manuel)
+        // =========================================================
+        public void RemplirComboEtat()
+        {
+            cbEtat.Items.Clear();
+            cbEtat.Items.Add("Disponible");
+            cbEtat.Items.Add("Occupée");
+            cbEtat.Items.Add("Hors service");
+        }
+
+        // =========================================================
+        // 3. REMPLIR GRID (Table 'chambres')
+        // =========================================================
+        public void ActualiserGrid()
+        {
+            if (cn.State == ConnectionState.Open) cn.Close();
+            cn.Open();
+
+            // NOM DE TABLE : chambres (Pluriel)
+            string sql = "SELECT * FROM chambres";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+
+            dgvChambres.DataSource = dt;
+
+            // Masquer l'ID catégorie si nécessaire
+            if (dgvChambres.Columns.Contains("id_categorie"))
+            {
+                dgvChambres.Columns["id_categorie"].Visible = false;
+            }
+
+            cn.Close();
+
+            // Mettre à jour le compteur
+            AfficherNombreChambres();
+        }
+
+        // =========================================================
+        // 4. BOUTON AJOUTER
+        // =========================================================
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Open) cn.Close();
+                cn.Open();
+
+                // Concaténation + Table 'chambres'
+                // Attention au prix : Replace ',' par '.'
+                string sql = "INSERT INTO chambres (numero, prix, etat, id_categorie) VALUES ('"
+                                + txtNumero.Text + "', "
+                                + txtPrix.Text.Replace(",", ".") + ", '"
+                                + cbEtat.Text + "', "
+                                + cbCategorie.SelectedValue + ")";
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+
+                MessageBox.Show("Chambre ajoutée avec succès !");
+                ActualiserGrid();
+                ViderChamps();
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show("Erreur Ajout : " + ex.Message);
+            }
+        }
+
+        // =========================================================
+        // 5. BOUTON RECHERCHER
+        // =========================================================
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Open) cn.Close();
+                cn.Open();
+
+                string sql = "SELECT * FROM chambres WHERE numero = '" + txtNumero.Text + "'";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txtPrix.Text = dr["prix"].ToString();
+                    cbEtat.Text = dr["etat"].ToString();
+                    cbCategorie.SelectedValue = dr["id_categorie"];
+                }
+                else
+                {
+                    MessageBox.Show("Aucune chambre trouvée avec ce numéro.");
+                }
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // =========================================================
+        // 6. BOUTON MODIFIER
+        // =========================================================
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Open) cn.Close();
+                cn.Open();
+
+                string sql = "UPDATE chambres SET " +
+                             "prix = " + txtPrix.Text.Replace(",", ".") + ", " +
+                             "etat = '" + cbEtat.Text + "', " +
+                             "id_categorie = " + cbCategorie.SelectedValue + " " +
+                             "WHERE numero = '" + txtNumero.Text + "'";
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+                int res = cmd.ExecuteNonQuery();
+                cn.Close();
+
+                if (res > 0)
+                {
+                    MessageBox.Show("Modification effectuée.");
+                    ActualiserGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Chambre introuvable (Vérifiez le numéro).");
+                }
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show("Erreur Modif : " + ex.Message);
+            }
+        }
+
+        // =========================================================
+        // 7. BOUTON SUPPRIMER
+        // =========================================================
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // On récupère le numéro soit de la grille, soit du textbox
+                string num = txtNumero.Text;
+                if (dgvChambres.CurrentRow != null && dgvChambres.CurrentRow.Selected)
+                {
+                    num = dgvChambres.CurrentRow.Cells["numero"].Value.ToString();
+                }
+
+                if (string.IsNullOrEmpty(num))
+                {
+                    MessageBox.Show("Veuillez sélectionner une chambre.");
+                    return;
+                }
+
+                if (MessageBox.Show("Voulez-vous supprimer la chambre " + num + " ?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    if (cn.State == ConnectionState.Open) cn.Close();
+                    cn.Open();
+
+                    string sql = "DELETE FROM chambres WHERE numero = '" + num + "'";
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+
+                    ActualiserGrid();
+                    ViderChamps();
+                }
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show("Erreur Suppression : " + ex.Message);
+            }
+        }
+
+        // =========================================================
+        // 8. BOUTON VIDER
+        // =========================================================
+        private void btnVider_Click(object sender, EventArgs e)
+        {
+            ViderChamps();
+        }
+
+        private void ViderChamps()
+        {
+            txtNumero.Clear();
+            txtPrix.Clear();
+            cbEtat.SelectedIndex = -1;
+            cbCategorie.SelectedIndex = -1;
+            txtNumero.Focus();
+        }
+
+        // =========================================================
+        // 9. COMPTEUR (Label)
+        // =========================================================
+        private void AfficherNombreChambres()
+        {
+            if (cn.State == ConnectionState.Open) cn.Close();
+            cn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT COUNT(*) FROM chambres", cn);
+            int nombre = Convert.ToInt32(cmd.ExecuteScalar());
+
+            lblTotal.Text = nombre.ToString();
+
+            cn.Close();
+        }
+
+    }
+}
